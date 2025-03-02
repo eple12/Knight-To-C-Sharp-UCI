25b2b05a2524568675e181346bef959f19567bb0
kms <marlon12@naver.com>
2025-03-02 16:02:09 +0900

    Initial Unchanged Tuned Parameters

--- Knight-To-C-Sharp-UCI/Scripts/Evaluation/EvaluationConstants.cs
+++ Knight-To-C-Sharp-UCI/Scripts/Evaluation/EvaluationConstants.cs
@@ -27,39 +27,38 @@ public static class EvaluationConstants
     public const int CheckmateEval = 99999;
 
     // Material
-    public static readonly Score[] MaterialValues = { S(100, 100), S(320, 320), S(325, 325), S(500, 500), S(900, 900) };
+    public static readonly Score[] MaterialValues = { S(101, 149), S(472, 557), S(441, 516), S(522, 904), S(1433, 1819) };
 
     // Piece Mobility
-    public static readonly Score PieceMobilityPerSquare = S(1, 1);
+    public static readonly Score PieceMobilityPerSquare = S(8, 6);
 
     // Outpost
-    public static readonly Score OutpostBonus = S(20, 20);
+    public static readonly Score OutpostBonus = S(23, 29);
 
     // Mop-Up
-    public static readonly Score CloserToEnemyKing = S(0, 8);
-    public static readonly Score EnemyKingCorner = S(0, 6);
-    public static readonly Score EnemyKingFriendlyBishopSquare = S(0, 8);
+    public static readonly Score CloserToEnemyKing = S(1, -4);
+    public static readonly Score EnemyKingCorner = S(-13, 14);
+    public static readonly Score EnemyKingFriendlyBishopSquare = S(0, 0);
 
     // Open File
-    public static readonly Score RookOnOpenFileBonus = S(20, 20);
-    public static readonly Score RookOnSemiOpenFileBonus = S(20, 20);
+    public static readonly Score RookOnOpenFileBonus = S(39, 7);
+    public static readonly Score RookOnSemiOpenFileBonus = S(16, 6);
 
     // Pawn structure
     public static readonly Score[] PassedPawnBonus = 
-    { S(0, 0), S(120, 120), S(80, 80), S(60, 60), S(40, 40), S(30, 30), S(15, 15), S(15, 15) };
+    { S(0, 0), S(99, 256), S(80, 194), S(19, 101), S(-24, 64), S(-19, 22), S(-12, 14), S(15, 15) };
     public static readonly Score[] IsolatedPawnPenaltyByCount = 
-    { S(0, 0), S(10, 10), S(25, 25), S(50, 50), S(75, 75), S(75, 75), S(75, 75), S(75, 75), S(75, 75) };
-    // public static readonly Score SpaceAdvantagePerSquare = S(1, 1);
+    { S(-15, -18), S(2, 4), S(24, 26), S(64, 44), S(85, 72), S(77, 122), S(118, 157), S(132, 197), S(75, 74) };
 
     // King Safety
     public static readonly Score[] VirtualKingMobilityBonus = {
-        S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), 
-        S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), 
-        S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0), S(0, 0)
+        S(61, -132), S(115, -111), S(110, -97), S(98, -98), S(83, -82), S(73, -73), S(66, -65), S(57, -63), S(52, -49), S(29, -30), 
+        S(8, -19), S(-16, -5), S(-44, 5), S(-81, 17), S(-110, 26), S(-145, 35), S(-161, 39), S(-193, 46), S(-187, 46), S(-187, 44), 
+        S(-230, 52), S(-230, 47), S(-233, 59), S(-221, 55), S(-245, 66), S(-190, 61), S(-125, 53)
     };
 
-    public static readonly Score KingOpenFilePenalty = S(-60, 0);
-    public static readonly Score KingSemiOpenFilePenalty = S(-30, 0);
+    public static readonly Score KingOpenFilePenalty = S(-68, 33);
+    public static readonly Score KingSemiOpenFilePenalty = S(-18, 42);
 
-    public static readonly Score KingShieldBonus = S(15, 0);
+    public static readonly Score KingShieldBonus = S(14, 10);
 }
